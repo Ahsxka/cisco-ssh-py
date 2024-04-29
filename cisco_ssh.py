ff64a7f04e39f28002c92bb69666778a842db255
Ahsxka <162576190+Ahsxka@users.noreply.github.com>
2024-04-29 12:36:32 +0200

    added support for GUI

--- cisco_ssh.py
+++ cisco_ssh.py
@@ -7,6 +7,7 @@ from netmiko import ConnectHandler, NetMikoTimeoutException, NetMikoAuthenticati
 import os
 
 
+# Testing submodules
 ######################################
 #               COLORS               #
 ######################################
